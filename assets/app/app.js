53d6be58e17cdbbfd7dc8eae262e860d95d2caea
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-03-17 23:00:23 +0100

    Refocor and resconstruct

---
+++ assets/app/app.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var app = angular.module(
+    'mainApp', [
+        'ngRoute',
+        'ngAnimate',
+        'app.main'
+    ],
+//    function($interpolateProvider) {
+//        /* INTERPOLATION
+//        * Normal Angular {{}} now becomes {[{}]} so take care. */
+//        $interpolateProvider.startSymbol('{[{');
+//        $interpolateProvider.endSymbol('}]}');
+//    });
+
+/**
+ * Config
+ */
+app.config(function($locationProvider, $routeProvider) {
+    /*
+     * Enabled HTML5 mode. Probably will not support
+     * any browser especially < IE10
+     */
+    $locationProvider.html5Mode(true);
+
+    /*
+     * Routes for the mainApp
+     */
+    $routeProvider.when('/', {
+        templateUrl: '/assets/app/views/main.html',
+        controller:  'mainAppController'
+    }).otherwise({redirectTo: '/404.html'}); // stub for production
+});
+
+
+/**
+ * Controller
+ */
+app.controller('mainAppController', function($scope, $location) {
+    // Nothing for now. Maybe some static stuff
+//    $scope.go_to_list = function(){
+//        $location.path('/list/');
+//    }
+});
