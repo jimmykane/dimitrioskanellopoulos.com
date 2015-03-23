0f7b918e6cfc255d2a2427f5a129a474c84ff1c8
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-03-23 14:32:12 +0100

    Switch to http resrouce

--- assets/app/services/json.js
+++ assets/app/services/json.js
@@ -1,6 +1,5 @@
 "use strict";
 
 angular.module('app.jsonService', ['ngResource']).factory('jsonService', function($resource) {
-    alert(1);
-  return $resource('/assets/app/data/info.json');
+    //return $resource('/assets/app/data/info.json');
 });
