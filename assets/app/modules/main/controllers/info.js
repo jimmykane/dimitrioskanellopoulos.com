0656b416c76ada8b51c51de229f95149399fa7c8
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-03-23 16:10:12 +0100

    Add @todo

--- assets/app/modules/main/controllers/info.js
+++ assets/app/modules/main/controllers/info.js
@@ -2,7 +2,7 @@
 
 angular.module('app.main').controller('infoController', function($scope, $http) {
 
-    // Query here because its an array and get expects an object
+    // @todo check if this should be a resource
     $http.get('/assets/app/data/info.json')
         .success(function(data, status, headers, config) {
             $scope.links = data;
