07a2c37542c32e8a63aec3e597041f8118eefde6
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-03-25 20:09:26 +0100

    Add runkeeper secrets

--- config/config.py
+++ config/config.py
@@ -1,25 +1,33 @@
 project = {
-    "title": "Dimitrios Kanellopoulos",
-    "domain": "dimitrioskanellopoulos.com",
-    "meta": {
-        "description": "My personal website",
-        "keywords": "Dimitrios Kanellopoulos, Dimitrios, Kanellopoulos",
-        "author": "Dimitrios Kanellopoulos",
-        "og": {
-            "title": "Dimitrios Kanellopoulos",
-            "description": "My personal website",
-            "type": "website",
-            "url": "http://dimitrioskanellopoulos.com",
-            "image": "https://www.gravatar.com/avatar/50eda10b9e16333f9e9cd3b8f1e0918a?s=500&d=identicon&r=PG"
-        }
+    'title': 'Dimitrios Kanellopoulos',
+    'domain': 'dimitrioskanellopoulos.com',
+    'meta': {
+        'description': 'My personal website',
+        'keywords': 'Dimitrios Kanellopoulos, Dimitrios, Kanellopoulos',
+        'author': 'Dimitrios Kanellopoulos',
+        'og': {
+            'title': 'Dimitrios Kanellopoulos',
+            'description': 'My personal website',
+            'type': 'website',
+            'url': 'http://dimitrioskanellopoulos.com',
+            'image': 'https://www.gravatar.com/avatar/50eda10b9e16333f9e9cd3b8f1e0918a?s=500&d=identicon&r=PG'
+        },
     },
-    "api_keys": {
-        "google_analytics": "UA-61188889-1"
+    'api_keys': {
+        'google_analytics': 'UA-61188889-1',
+        'runkeeper': {
+            'client_id': 'af2312a9c256448394b6782279842339',
+            'client_secret': 'dc43a89ef2d045d6a6323a41132b553c',
+            'urls': {
+                 'authorization_url': 'https://runkeeper.com/apps/authorize',
+                 'access_token_url': 'https://runkeeper.com/apps/token',
+                 'deauthorization_url': 'https://runkeeper.com/apps/de-authorize',
+            },
+        },
     }
 }
 
 
-
 config = {
-    "project": project,
+    'project': project,
 }
