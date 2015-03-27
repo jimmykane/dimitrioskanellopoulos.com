1b28499633d06b39ba388dfe6a6daf7e88b3f472
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-03-27 13:49:09 +0100

    Update app config to use libs

--- appengine_config.py
+++ appengine_config.py
@@ -1,7 +1,7 @@
 """This file is loaded when starting a new application instance."""
-import sys
-import os.path
 
-# add `lib` subdirectory to `sys.path`, so our `main` module can load
-# third-party libraries.
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+from google.appengine.ext import vendor
+
+
+# Add any libraries installed in the "lib" folder.
+vendor.add('lib')
