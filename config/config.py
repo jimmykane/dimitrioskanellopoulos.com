b302b221cd4a5ff0665a341e88ee0583e2d05435
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-03-28 19:01:05 +0100

    Remove API keys
    They where reset anyways so no leak ;-)

--- config/config.py
+++ config/config.py
@@ -13,19 +13,6 @@ project = {
             'image': 'https://www.gravatar.com/avatar/50eda10b9e16333f9e9cd3b8f1e0918a?s=500&d=identicon&r=PG'
         },
     },
-    'api_keys': {
-        'google_analytics': 'UA-61188889-1',
-        'runkeeper': {
-            'client_id': 'af2312a9c256448394b6782279842339',
-            'client_secret': 'dc43a89ef2d045d6a6323a41132b553c',
-            'urls': {
-                 'redirect_uri': 'http://localhost:8080/auth/runkeeper_callback',
-                 'authorization_url': 'https://runkeeper.com/apps/authorize',
-                 'access_token_url': 'https://runkeeper.com/apps/token',
-                 'deauthorization_url': 'https://runkeeper.com/apps/de-authorize',
-            },
-        },
-    }
 }
 
 
