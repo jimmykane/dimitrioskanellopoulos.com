a64386ddb1b39e47450176ad855dbb6910494351
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-03-28 21:04:41 +0100

    Improve implementation

--- controllers/humanapi.py
+++ controllers/humanapi.py
@@ -7,14 +7,24 @@ import webapp2
 from google.appengine.api import urlfetch
 from config.config import get_api_keys
 from externalapis import humanapi
+from webapp2 import uri_for
 
-class HumanAPIAuthHandler(webapp2.RequestHandler):
+class HumanAPIAuthHandler(object):
+
+    @classmethod
+    def get_humanapi_auth(self):
+         return humanapi.Auth(
+            client_id=get_api_keys()['humanapi']['client_id'],
+            client_secret=get_api_keys()['humanapi']['client_secret'])
+
+
+class HumanAPIAuthCallHandler(HumanAPIAuthHandler, webapp2.RequestHandler):
 
     def get(self):
-        self.redirect(self.get_auth_url())
+        self.redirect(self.get_humanapi_auth().get_authorize_url('http://localhost:8080/auth/humanapi_auth_callback'))
 
-    def get_auth_url(self):
-        return humanapi.Auth(
-            client_id=get_api_keys()['humanapi']['client_id'],
-            client_secret=get_api_keys()['humanapi']['client_secret'])\
-            .get_authorize_url('http://localhost:8080/auth/humanapi_auth_callback')
+
+class HumanAPIAuthCallBackHandler(webapp2.RequestHandler):
+
+    def get(self):
+        pass
