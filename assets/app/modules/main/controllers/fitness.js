4b02fdb19f6eaf8dd39b791a3291d01137d477ec
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-04-16 22:32:55 +0200

    Fix implementation

--- assets/app/modules/main/controllers/fitness.js
+++ assets/app/modules/main/controllers/fitness.js
@@ -5,16 +5,19 @@ angular.module('app.main').controller('fitnessController', function($scope, $htt
     // Stub here to get my personal data
     var userId = '29509824';
 
+    $scope.metrics = [];
+
     // @todo check if this should be a resource
 
-
     // Get some
-    $http.get('/metrics/runkeeper/' + userId + '/records')
+    $http.get('/metrics/runkeeper/' + userId + '/weight')
         .success(function(data, status, headers, config) {
             debugger;
-            $scope.metrics = data;
+            // Get the 1st one
+            $scope.metrics.push({weight: data.items[0].weight});
         })
         .error(function(data, status, headers, config) {
             // log error
         });
+
 });
