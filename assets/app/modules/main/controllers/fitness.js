068b698dce7a910b049db4c311119794b35f5a43
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-04-19 23:45:22 +0200

    Implement service

--- assets/app/modules/main/controllers/fitness.js
+++ assets/app/modules/main/controllers/fitness.js
@@ -7,30 +7,7 @@ angular.module('app.main').controller('fitnessController', function($scope, $htt
 
     $scope.metrics = [];
 
-    // @todo check if this should be a resource and moved to service and promise
-
-    // Get some weight
-    $http.get('/metrics/runkeeper/' + userId + '/weight')
-        .success(function(data, status, headers, config) {
-            // Find weight and fat percentage if possible
-            var weight;
-            var fatPercent;
-            for (var weightMeasurement in data.items){
-                if (!weight && data.items[weightMeasurement].weight){
-                    weight = data.items[weightMeasurement].weight;
-                }
-                if (!fatPercent && data.items[weightMeasurement].fat_percent){
-                    fatPercent = data.items[weightMeasurement].fat_percent;
-                }
-            }
-            $scope.metrics.push({
-                Weight: weight + 'Kg',
-                'Body Fat': fatPercent + '%'
-            });
-        })
-        .error(function(data, status, headers, config) {
-            // log error
-        });
+    // @todo check to inject weight via service
 
     // Get latest activity
     $http.get('/metrics/runkeeper/' + userId + '/fitnessActivities')
--- assets/app/modules/main/services/fitness.js
+++ assets/app/modules/main/services/fitness.js
@@ -1,8 +1,45 @@
 "use strict";
 
+
+//@todo Check it this belongs to this main modules
 angular.module('app.main').factory('fitnessService', function($http, $q) {
 
     var fitnessService = {};
+    fitnessService.weightMeauremnts = {};
+
+
+    fitnessService.getUserWeightMeasurements = function(user){
+
+        // Get some weight
+        $http.get('/metrics/runkeeper/' + userId + '/weight')
+        .success(function(response, status, headers, config) {
+            if (response.status.code !== 200){
+                deffered.resolve(response.status);
+                return;
+            }
+
+            var weight;
+            var fatPercent;
+            for (var weightMeasurement in response.items){
+                if (!weight && response.items[weightMeasurement].weight){
+                    weight = response.items[weightMeasurement].weight;
+                }
+                if (!fatPercent && response.items[weightMeasurement].fat_percent){
+                    fatPercent = response.items[weightMeasurement].fat_percent;
+                }
+            }
+            fitnessService.weightMeauremnts.weight = weight + 'Kg';
+            fitnessService.weightMeauremnts.bodyFat =  fatPercent + '%';
+
+            deffered.resolve(response.status);
+        })
+        .error(function(response, status, headers, config) {
+            deffered.reject(response.status);
+            console.log(response, status, headers, config);
+        });
+        return deffered.promise;
+    };
+
 
 
     return fitnessService;
