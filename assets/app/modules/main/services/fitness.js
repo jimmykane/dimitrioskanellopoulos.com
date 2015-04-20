1f2df09004b7e7af647a33f7921c05697d026712
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-04-20 16:20:35 +0200

    Bug fixes

--- assets/app/modules/main/services/fitness.js
+++ assets/app/modules/main/services/fitness.js
@@ -2,45 +2,48 @@
 
 
 //@todo Check it this belongs to this main modules
-angular.module('app.main').factory('fitnessService', function($http, $q) {
+angular.module('app.main').factory('fitnessService', function ($http, $q) {
 
     var fitnessService = {};
-    fitnessService.weightMeauremnts = {};
+    var weightMeasurements = {};
 
 
-    fitnessService.getUserWeightMeasurements = function(user){
-
+    fitnessService.getUserWeightMeasurements = function (userId) {
+        var deffered = $q.defer();
         // Get some weight
         $http.get('/metrics/runkeeper/' + userId + '/weight')
-        .success(function(response, status, headers, config) {
-            if (response.status.code !== 200){
-                deffered.resolve(response.status);
-                return;
-            }
-
-            var weight;
-            var fatPercent;
-            for (var weightMeasurement in response.items){
-                if (!weight && response.items[weightMeasurement].weight){
-                    weight = response.items[weightMeasurement].weight;
+            .success(function (data, status, headers, config) {
+                debugger;
+                if (status !== 200) {
+                    deffered.resolve(data.status);
+                    return;
                 }
-                if (!fatPercent && response.items[weightMeasurement].fat_percent){
-                    fatPercent = response.items[weightMeasurement].fat_percent;
-                }
-            }
-            fitnessService.weightMeauremnts.weight = weight + 'Kg';
-            fitnessService.weightMeauremnts.bodyFat =  fatPercent + '%';
 
-            deffered.resolve(response.status);
-        })
-        .error(function(response, status, headers, config) {
-            deffered.reject(response.status);
-            console.log(response, status, headers, config);
-        });
+                var weight;
+                var fatPercent;
+                for (var weightMeasurement in data.items) {
+                    if (!weight && data.items[weightMeasurement].weight) {
+                        weight = data.items[weightMeasurement].weight;
+                    }
+                    if (!fatPercent && data.items[weightMeasurement].fat_percent) {
+                        fatPercent = data.items[weightMeasurement].fat_percent;
+                    }
+                }
+                weightMeasurements.weight = weight + 'Kg';
+                weightMeasurements.fatPercent = fatPercent + '%';
+
+                deffered.resolve(status);
+            })
+            .error(function (data, status, headers, config) {
+                deffered.reject(status);
+                console.log(data, status, headers, config);
+            });
         return deffered.promise;
     };
 
-
+    fitnessService.weightMeasurements = function(){
+        return weightMeasurements;
+    };
 
     return fitnessService;
 
