84bf8876e8ddf6fc4710185e0a1fc692c70607ce
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-05-03 18:38:51 +0200

    Switch to decorator for connivance

--- controllers/login.py
+++ controllers/login.py
@@ -2,15 +2,13 @@ from models.users import UserModel
 
 import webapp2
 from google.appengine.api import users
-
+from webapp2_extras.appengine.users import login_required
 
 
 class LoginHandler(webapp2.RequestHandler):
+
+    @login_required
     def get(self):
         user = users.get_current_user()
-        if not user:
-            self.redirect(users.create_login_url(self.request.uri), abort=True)
-            return
-
         user = UserModel.get_or_insert(user.user_id())
     pass
