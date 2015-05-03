8f4c124d880d6cf0475622407fbe566ae0bf3a6b
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-05-03 19:19:18 +0200

    Create auth model for google plus

--- models/auth.py
+++ models/auth.py
@@ -1,10 +1,10 @@
 import logging
 from models.ndb_models import *
 from google.appengine.ext import ndb
+from oauth2client.appengine import CredentialsProperty
 
 
 class AuthenticationModel(ndb.Expando, DictModel, NDBCommonModel):
-    access_token = ndb.StringProperty(required=True)
     creation_date = ndb.DateTimeProperty(auto_now_add=True)
     edit_date = ndb.DateTimeProperty(auto_now=True)
 
@@ -12,3 +12,7 @@ class AuthenticationModel(ndb.Expando, DictModel, NDBCommonModel):
 class RunkeeperAuthModel(AuthenticationModel):
     access_token = ndb.StringProperty(required=True)
     access_token_type = ndb.StringProperty(required=True)
+
+
+class GooglePlusAuthModel(AuthenticationModel):
+    credentials = CredentialsProperty(required=True)
