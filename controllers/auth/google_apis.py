e5536ff0b47738df5447480ac05a978ea62ddd6d
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-05-03 19:26:13 +0200

    Temp

--- controllers/auth/google_apis.py
+++ controllers/auth/google_apis.py
@@ -1,11 +1,14 @@
 import webapp2
 import pickle,json
 
+from models.users import GooglePlusUserModel
+
 from webapp2 import uri_for
 from config.config import get_client_secrets_filename
 from apiclient.discovery import build
 
 from oauth2client.client import flow_from_clientsecrets
+from webapp2_extras.appengine.users import login_required
 
 
 
@@ -23,12 +26,14 @@ class GoogleAuthHandler(webapp2.RequestHandler):
 
 class GoogleAuthCallHandler(GoogleAuthHandler):
 
+    @login_required
     def get(self, scope):
         self.redirect(str(self.get_oauth2_flow().step1_get_authorize_url()))
         pass
 
 
 class GoogleAuthCallbackHandler(GoogleAuthHandler):
+    @login_required
     def get(self):
         if self.request.params.get('error'):
             self.request.response.out.write(self.request.params.get('error'))
@@ -39,38 +44,6 @@ class GoogleAuthCallbackHandler(GoogleAuthHandler):
         # Get the auth session
         google_credentials = self.get_oauth2_flow().step2_exchange(self.request.params.get('code'))
         service = build('plus', 'v1', credentials=google_credentials)
-        google_request = service.people().get(userId='me').execute()
-        pass
-        # runkeeper_api = RunkeeperAPI(
-        #     access_token=access_token,
-        #     access_token_type=access_token_type,
-        #     debug=True
-        # )
-        # runkeeper_user = runkeeper_api.get_user()
-        # # test = runkeeper_user.call('')
-        # runkeeper_user_profile = runkeeper_user.profile()
-        #
-        # # Get or insert the model update tokens etc
-        # runkeeper_auth_model = RunkeeperUserModel.get_or_insert(
-        #     str(runkeeper_user.user_id),
-        #     access_token=access_token,
-        #     access_token_type=access_token_type,
-        #     name=runkeeper_user_profile['name'],
-        #     profile=runkeeper_user_profile['profile'],
-        #     large_picture=runkeeper_user_profile.get('large_picture')
-        # )
-        #
-        # # Update
-        # runkeeper_auth_model.populate(
-        #     access_token=access_token,
-        #     access_token_type=access_token_type,
-        #     name=runkeeper_user_profile['name'],
-        #     profile=runkeeper_user_profile['profile'],
-        #     large_picture=runkeeper_user_profile.get('large_picture')
-        # )
-        #
-        # # Write blind again
-        # runkeeper_auth_model.put()
-
-        # Write the result
+        google_plus_profile = service.people().get(userId='me').execute()
+        google_plus_user =
         self.response.out.write('Success')
