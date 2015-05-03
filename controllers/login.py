55c5e44d1b49c63710a84e7ab7fce83bfe5513c1
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-05-03 17:39:29 +0200

    Login procedure without success redirect

--- controllers/login.py
+++ controllers/login.py
@@ -1,5 +1,16 @@
+from models.users import UserModel
+
 import webapp2
 from google.appengine.api import users
 
+
+
 class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if not user:
+            self.redirect(users.create_login_url(self.request.uri), abort=True)
+            return
+
+        user = UserModel.get_or_insert(user.user_id())
     pass
