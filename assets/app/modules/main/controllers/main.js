8f74f778c6bc7a7aeb069bb77f60c659a612d805
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-05-04 14:12:34 +0200

    Populate main info via google plus profile

--- assets/app/modules/main/controllers/main.js
+++ assets/app/modules/main/controllers/main.js
@@ -1,10 +1,21 @@
 "use strict";
 
-angular.module('app.main').controller('mainController', function ($scope, googlePlusService) {
+angular.module('app.main').controller('mainController', function ($scope, googlePlusService, $sce) {
 
     var googlePlusUserID = window.googlePlusUserID;
 
     // Profile
     $scope.googlePlusProfile = googlePlusService.getProfile(googlePlusUserID);
+
+    $scope.getAboutMe = function (){
+         return $sce.trustAsHtml($scope.googlePlusProfile.aboutMe);
+    };
+
+    $scope.getProfileImageUrl = function(){
+        if (!$scope.googlePlusProfile.image){
+            return;
+        }
+        return $scope.googlePlusProfile.image.url.slice(0, -2) + '100';
+    }
 
 });
