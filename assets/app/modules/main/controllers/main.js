aa516d494ce0199d01279a20ac807adb44770d61
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-05-04 13:17:50 +0200

    Inject google plus profile to main module

--- assets/app/modules/main/controllers/main.js
+++ assets/app/modules/main/controllers/main.js
@@ -1,5 +1,10 @@
 "use strict";
 
-angular.module('app.main').controller('mainController', function ($scope) {
+angular.module('app.main').controller('mainController', function ($scope, googlePlusService) {
+
+    var googlePlusUserID = window.googlePlusUserID;
+
+    // Profile
+    $scope.googlePlusProfile = googlePlusService.getProfile(googlePlusUserID);
 
 });
