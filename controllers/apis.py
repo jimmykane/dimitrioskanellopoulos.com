f7b9c0bf14bb9b1a143a5ba08027fccae829bf0f
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-05-04 11:34:07 +0200

    Add simple logic and build up for google plus api

--- controllers/apis.py
+++ controllers/apis.py
@@ -1,6 +1,9 @@
 import json
 
+
 from config.config import is_dev_server
+from lib.apis.google_plus_api import GooglePlusAPI
+from models.users import GooglePlusUserModel
 
 import webapp2
 
@@ -10,10 +13,17 @@ class GooglePlusAPIHandler(webapp2.RequestHandler):
 
     ]
 
-    def get(self, user_id, call, id_=None):
+    def get(self, user_id, call):
         # If its not allowed gto
         if not is_dev_server() and call in self.disallowed_calls:
             self.response.out.write('Call not allowed')
             return
+        google_plus_user_model = GooglePlusUserModel.get_by_id(user_id)
+        if not google_plus_user_model:
+            self.response.out.write('No user found')
+            return
+        google_plus_api = GooglePlusAPI(credentials=google_plus_user_model.credentials)
+        google_plus_user = GooglePlusAPI.get_user()
+        pass
 
 
--- lib/apis/google_plus_api.py
+++ lib/apis/google_plus_api.py
@@ -9,6 +9,7 @@ from apiclient.discovery import build
 class GooglePlusAPI(object):
     # Set by init
     credentials = None
+    google_plus_service = None
     debug_level = logging.DEBUG
 
     def __init__(self, credentials, debug=False):
@@ -18,6 +19,8 @@ class GooglePlusAPI(object):
         )
         self.debug_level = logging.INFO if debug else logging.DEBUG
         self.credentials = credentials
+        self.google_plus_service = build('plus', 'v1', credentials=credentials)
+
 
     def get_user(self):
         return GooglePlusUser(self)
@@ -28,48 +31,7 @@ class GooglePlusUser(object):
 
     def __init__(self, master):
         self.master = master
-        # Get the user methods and set the attributes
-        for user_method, call in self.master.query('/user').iteritems():
-            if user_method == 'userID':
-                # Assign the userID to the object
-                self.user_id = call
-                continue
-            # add them also with the camelcase
-            setattr(
-                self,
-                call[1:],
-                lambda id_=None, _call=call: self.master.query(_call, id_)
-            )
-
-        # Set the other attributes
-        # @todo feels this should be elsewhere
-        setattr(self, 'weightMeasurements', self.get_weight_measurements)
-        setattr(self, 'latestActivity', self.get_latest_activity)
+        self.user_id = master.google_plus_service.people().get(userId='me').execute()['id']
 
     def get_user_id(self):
         return self.user_id
-
-    """
-    Wrapper to pass the latest and save data
-    """
-
-    def get_latest_activity(self):
-        return self.fitnessActivities()['items'][0]
-
-    """
-    Wrapper to do partial logic and save data
-    """
-
-    def get_weight_measurements(self):
-        weight = None
-        fat_percent = None
-        # @todo optimize
-        for item in self.weight()['items']:
-            if not weight:
-                weight = item.get('weight')
-            if not fat_percent:
-                fat_percent = item.get('fat_percent')
-        return {
-            'weight': weight,
-            'fat_percent': fat_percent
-        }
