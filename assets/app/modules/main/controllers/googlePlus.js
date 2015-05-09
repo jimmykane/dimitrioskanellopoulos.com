0ad0f9d963ffcfdff158110822c62d1e6f9c89a3
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2015-05-09 12:00:31 +0200

    Add function to check if the profile is ready

--- assets/app/modules/main/controllers/googlePlus.js
+++ assets/app/modules/main/controllers/googlePlus.js
@@ -5,12 +5,17 @@ angular.module('app.main').controller('googlePlusController', function ($scope, 
      var googlePlusUserID = window.googlePlusUserID;
 
     // The whole profile
-    $scope.googlePlusProfile = googlePlusService.getProfile(googlePlusUserID);
+    $scope.profile = googlePlusService.profile || googlePlusService.getProfile(googlePlusUserID);
 
     // About me to be rendered via service
     $scope.getAboutMe = googlePlusService.getAboutMe;
 
     // Image to be resized via service
     $scope.getProfileImageUrl = googlePlusService.getProfileImageUrl;
+
+    // Check if it's ready (used to show/hide the html)
+    $scope.isProfileReady = function (){
+        return !angular.equals({}, $scope.profile);
+    };
 
 });
